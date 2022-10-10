08faff5e5cbd92da688dcf232ce9eee8a714f227
James <jamesarnott2@gmail.com>
2022-10-10 20:58:07 +0100

    feat: created initial keystone correction component

---
+++ web-interface/src/components/KeystoneCorrection.tsx
@@ -0,0 +1,39 @@
+import React, { useContext, useState } from 'react';
+import { Settings } from '@/App';
+
+const KeystoneCorrection = () => {
+  const settings = useContext(Settings);
+  const [enable, setEnable] = useState(settings.currentSettings.keystoneCorrection.enable);
+
+  const toggleKeystoneCorrection = () => {
+    settings.updateSettings({
+      updateSettings: settings.updateSettings,
+      currentSettings: {
+        ...settings.currentSettings,
+        keystoneCorrection: {
+          ...settings.currentSettings.keystoneCorrection,
+          enable: !enable,
+        },
+      },
+    });
+    setEnable(!enable);
+  };
+
+  return (
+    <div className="bg-gray-300 mt-4 mb-4 p-3 ">
+      <div className="text-2xl">Keystone Correction</div>
+      <hr className="bg-black h-1 rounded-full mt-3 mb-3" />
+      <div className="flex flex-row">
+        <div className="flex flex-col">
+          <div className="text-xl">Enable Keystone Correction</div>
+          <div className="text-sm">Enable keystone correction to compensate for the distortion of the laser beam.</div>
+        </div>
+        <div className="flex flex-col">
+          <input type="checkbox" checked={enable} onChange={() => toggleKeystoneCorrection()} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default KeystoneCorrection;
